1b105e2adbd5bdec0e93c7c0712b7a22d5bc05d3
Susheel <v-voora1@ti.com>
2024-07-18 14:11:17 +0530

    Add tsn-l2 and tsn-lldp libs for am62dx
    Fixes: SITSW-3737
    
    Signed-off-by: Susheel <v-voora1@ti.com>

--- tools/create_eval_libs.sh
+++ tools/create_eval_libs.sh
@@ -15,14 +15,17 @@ make -C ../eval_src/ -f makefile.tsn_lldp_freertos.am64x.r5f.ti-arm-clang PROFIL
 
 make -C ../eval_src/ -f makefile.tsn_l2_freertos.am62ax.r5f.ti-arm-clang  PROFILE=release -s
 make -C ../eval_src/ -f makefile.tsn_l2_freertos.am62px.wkup-r5f.ti-arm-clang PROFILE=release -s
+make -C ../eval_src/ -f makefile.tsn_l2_freertos.am62dx.r5f.ti-arm-clang PROFILE=release -s
 make -C ../eval_src/ -f makefile.tsn_lldp_freertos.am62ax.r5f.ti-arm-clang PROFILE=release -s
 make -C ../eval_src/ -f makefile.tsn_lldp_freertos.am62px.wkup-r5f.ti-arm-clang PROFILE=release -s
+make -C ../eval_src/ -f makefile.tsn_lldp_freertos.am62dx.r5f.ti-arm-clang PROFILE=release -s
 
 mv ../eval_src/lib/tsn_l2-freertos.am243x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am243x.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_l2-freertos.am263x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am263x.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_l2-freertos.am64x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am64x.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_l2-freertos.am62ax.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am62ax.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_l2-freertos.am62px.wkup-r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am62px.wkup-r5f.ti-arm-clang.lib
+mv ../eval_src/lib/tsn_l2-freertos.am62dx.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_l2-freertos.am62dx.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_lldp-freertos.am243x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am243x.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_lldp-freertos.am263px.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am263px.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_lldp-freertos.am263x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am263x.r5f.ti-arm-clang.lib
@@ -30,6 +33,7 @@ mv ../eval_src/lib/tsn_lldp-freertos.am273x.r5f.ti-arm-clang.release.lib ../eval
 mv ../eval_src/lib/tsn_lldp-freertos.am64x.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am64x.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_lldp-freertos.am62ax.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am62ax.r5f.ti-arm-clang.lib
 mv ../eval_src/lib/tsn_lldp-freertos.am62px.wkup-r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am62px.wkup-r5f.ti-arm-clang.lib
+mv ../eval_src/lib/tsn_lldp-freertos.am62dx.r5f.ti-arm-clang.release.lib ../eval_src/lib/tsn_lldp-freertos.am62dx.r5f.ti-arm-clang.lib
 
 cp -f -r ../eval_src/lib/* ../eval_lib/
 
